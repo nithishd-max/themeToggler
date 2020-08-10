38c46a3e18eab6081613e9fae71195adc8ff935b
nithishdnr <nithishdnr3@gmail.com>
2020-08-10 20:12:11 +0530

    first commit

---
+++ src/Component/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import ThemeToggler from './ThemeToggler';
+// import HeroSection from "./HeroSection";
+// import ThemeContext from "../Context/ThemeContext";
+
+const Header =() =>{
+    return(
+        <header>
+    <h1>
+        theme Toggler
+    </h1>
+    <ThemeToggler/>
+</header>
+    )
+}
+export default Header;
